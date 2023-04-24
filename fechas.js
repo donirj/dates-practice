e0a90d5be749c570d53e33a1808a84e87a898231
Andoni <andoniresendiz@gmail.com>
2023-04-24 17:04:28 -0600

    first commit

---
+++ fechas.js
@@ -0,0 +1,18 @@
+const todayDate = new Date()
+console.log(todayDate)
+
+const myBirthDate = new Date('1994', '00', '02')
+console.log(myBirthDate)
+
+const isTodayLater = todayDate > myBirthDate
+console.log(isTodayLater)
+
+const myBirthday = new Date('1994', '00', '02')
+console.log(myBirthday)
+
+const myBirthMonth = new Date('1994', '00')
+console.log(myBirthMonth)
+
+const myBirthYear = new Date('1994')
+console.log(myBirthYear)
+
